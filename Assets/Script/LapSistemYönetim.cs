2e267047ac7911bbbbf2d4eebcafb9b1ea4b06bb
emirralkn60 <emirralkn@gmail.com>
2025-03-10 14:31:03 +0300

    CarRacing
    CarRacing

---
+++ Assets/Script/LapSistemYönetim.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Linq;
+
+
+public class LapSistemYönetim : MonoBehaviour
+{
+    public List<GameObject> arabalar = new List<GameObject>();
+    public TextMeshProUGUI sira;
+
+
+    private void Start()
+    {
+        sira.text = "1/3";        
+    }
+    public void LapGonder(GameObject gelenobje)
+    {
+        if(gelenobje.name=="Biz")
+        {
+            
+            gelenobje.GetComponent<Siralama>().pozisyon = arabalar.Count() + 1;
+        }
+        else
+        {
+            arabalar.Add(gelenobje);
+        }
+        
+
+    }
+    
+  
+    public void lapKontrolEt(int lapStep)
+    {
+        sira.text = lapStep + "/3";
+    }
+}
---
+++ Assets/Script/Siralama.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+public class Siralama : MonoBehaviour
+{
+    public int AktifYonSirasi = 1;
+    SiralamaYonetim siralama;
+    Genelayarlar genelayarlar;
+    LapSistemYönetim lapSistem;
+    public int pozisyon;
+    public int lapStep ;
+    void Start()
+    {
+        lapStep = 1;
+        genelayarlar = GameObject.FindWithTag("OyunKontrol").GetComponent<Genelayarlar>();
+        genelayarlar.kendinigonder(gameObject);
+
+        if (genelayarlar.harıtanınAdı.ToString()=="Tur")
+        {
+            lapSistem = GameObject.FindWithTag("OyunKontrol").GetComponent<LapSistemYönetim>();
+            
+        }
+        else
+        {
+            siralama = GameObject.FindWithTag("OyunKontrol").GetComponent<SiralamaYonetim>();
+            siralama.kendinigonder(gameObject, AktifYonSirasi);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+        if(genelayarlar.harıtanınAdı.ToString()== "Yarış")
+        {
+            if (other.CompareTag("GidisYonuObje"))
+            {
+                AktifYonSirasi = int.Parse(other.transform.gameObject.name);
+                siralama.siralamaGuncelle(gameObject, AktifYonSirasi);
+
+            }
+        }
+        
+       if (gameObject.name=="Biz")
+       {
+         if (other.CompareTag("Finish"))
+            {
+                if (genelayarlar.harıtanınAdı.ToString() == "Tur")
+                {
+                    GetComponentInParent<CarController>().YonGidisİndex = 0;
+                    AktifYonSirasi = 1;
+                    if(lapStep<3)
+                    {
+
+                        lapStep++;
+                        lapSistem.lapKontrolEt(lapStep);
+                    }
+                    else
+                    {
+                        lapSistem.LapGonder(gameObject);
+                        genelayarlar.OyunSonu(pozisyon);
+                    }
+                    
+
+                }
+                else
+                {
+                    genelayarlar.OyunSonu(pozisyon);
+                }
+                
+
+            }
+
+       }
+
+        
+    }
+
+
+    
+}
